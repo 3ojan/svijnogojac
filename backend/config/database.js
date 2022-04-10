a28a867528ed93ab3c4d26e896c37b2446a34735
Bojan Ratkovic <3ojans@gmail.com>
2022-04-10 19:12:58 +0200

    heroku && atlas db

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
   // "url": "mongodb://admin:admin@127.0.0.1:8081/svinjgojac",
-  'url': 'mongodb://admin:admin@127.0.0.1:27017/svinjgojac?authSource=admin'
+  // 'url': 'mongodb://admin:admin@127.0.0.1:27017/svinjgojac?authSource=admin',
+  'url': 'mongodb+srv://admin:admin@cluster0.p4p5b.mongodb.net/svinjgojac?retryWrites=true&w=majority'
 }
