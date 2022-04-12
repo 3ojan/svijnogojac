4bf529af479f3ca9a2a77728df80b2fea6675f12
vkopic <kopic.vlado@gmail.com>
2022-04-12 14:29:18 +0200

    Fixes

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,16 @@ function isEmpty(str) {
   return !str || 0 === str.length
 }
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  app.use(express.static('build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  });
+}
 
 
 mongoose.set('useCreateIndex', true)
