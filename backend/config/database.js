178a5a29cd7fb1e887fe1e3d092ceb4a9df31fc5
vkopic <kopic.vlado@gmail.com>
2022-04-12 13:13:12 +0200

    Fix typo

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
   // "url": "mongodb://admin:admin@127.0.0.1:8081/svinjgojac",
   // 'url': 'mongodb://admin:admin@127.0.0.1:27017/svinjgojac?authSource=admin',
-  'url': 'mongodb+srv://admin:admin@cluster0.p4p5b.mongodb.net/svinjgojac?retryWrites=true&w=majority'
+  'url': 'mongodb+srv://admin:admin@cluster0.p4p5b.mongodb.net/svinjogojac?retryWrites=true&w=majority'
 }
