c6b74626d1a2f6cb5ce69e83b588d7199863b879
Bojan Ratkovic <3ojans@gmail.com>
2022-04-20 09:30:07 +0200

    merge fix

--- frontend/src/components/store/reducers/articleReducer.js
+++ frontend/src/components/store/reducers/articleReducer.js
@@ -12,6 +12,8 @@ const initialState = {
 
 let names = {};
 let articles = [];
+let categoryNames = {};
+let categories = [];
 
 const userReducer = (state = initialState, action) => {
   switch (action.type) {
@@ -56,6 +58,17 @@ export const getArticleName = (id) => {
     names[id] = item[0].name
   }
   return names[id];
+}
+
+export const getCategoryName = (id) => {
+  if (categoryNames[id]) {
+    return categoryNames[id];
+  }
+  const item = categories.filter(item => item._id === id);
+  if (item[0]) {
+    categoryNames[id] = item[0].name
+  }
+  return categoryNames[id];
 }
 
 export default userReducer
